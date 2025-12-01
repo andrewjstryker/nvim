2687aa95e07330a1af48f7a757cfc7b9d51bb270
Andrew Stryker <axs@sdf.org>
2025-11-30 18:08:49 -0800

    Add an init module

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- ~/.config/nvim/init.lua
+
+-- Leader keys early
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Wire hermetic env (adds rocks tree + LuaRocks paths)
+require("config.env")
+
+-- Minimal core
+require("config.util")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
